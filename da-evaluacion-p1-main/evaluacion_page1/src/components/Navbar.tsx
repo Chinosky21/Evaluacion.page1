d220ac2a597b92976e2443842c49b11cfc82297e
Chinosky <luisernestonieves@gmail.com>
2025-04-10 23:24:29 -0600

    Se creo el componente navbar con su formato css

---
+++ da-evaluacion-p1-main/evaluacion_page1/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return(
+    <div>
+        <nav>
+            <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Nosotros</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Contactos</a></li>
+            </ul>
+        </nav>
+    </div>
+    )
+    }
+export default Navbar;
--- da-evaluacion-p1-main/evaluacion_page1/src/main.tsx
+++ da-evaluacion-p1-main/evaluacion_page1/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './components/Navbar.css'
 import Description from './components/Description';
-
+import Navbar from './components/Navbar';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Navbar/>
     <Description/>
+
   </StrictMode>,
 )
